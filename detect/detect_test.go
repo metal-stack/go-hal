4726ffc0a42dee6cdf0dc110eee99d8a5941f39e
Stefan Majer <stefan.majer@f-i-ts.de>
2020-05-12 17:30:24 +0200

    comment unused test to make linter happy

--- detect/detect_test.go
+++ detect/detect_test.go
@@ -1,34 +1,27 @@
 package detect
 
-import (
-	"reflect"
-	"testing"
-
-	"github.com/metal-stack/go-hal/pkg/api"
-)
-
-func _TestDetectInBand(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    api.Vendor
-		wantErr bool
-	}{
-		{
-			name:    "simple",
-			want:    api.VendorLenovo,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := inBand()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DetectInBand() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("DetectInBand() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func _TestDetectInBand(t *testing.T) {
+// 	tests := []struct {
+// 		name    string
+// 		want    api.Vendor
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name:    "simple",
+// 			want:    api.VendorLenovo,
+// 			wantErr: false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := inBand()
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("DetectInBand() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("DetectInBand() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
