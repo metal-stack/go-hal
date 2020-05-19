0f399f2606047e014461e24054ea64ef33417f26
Sandro Koll <sandro.koll@f-i-ts.de>
2020-05-19 09:25:56 +0200

    fake inband board if dmi retrieval failed

--- detect/detect.go
+++ detect/detect.go
@@ -16,21 +16,44 @@ var (
 )
 
 // inBand will try to detect the board vendor
-func inBand() (*api.Board, error) {
+func inBand() *api.Board {
 	b, err := dmi.BoardInfo()
 	if err != nil {
-		return nil, err
+		b = &api.Board{
+			VendorString: "vagrant",
+			Vendor:       0,
+			Model:        "vagrant",
+			PartNumber:   "vagrant",
+			SerialNumber: "vagrant",
+			BiosVersion:  "0",
+			BMC:          &api.BMC{
+				IP:                  "1.1.1.1",
+				MAC:                 "aa:bb:cc:dd:ee:ff",
+				ChassisPartNumber:   "vagrant",
+				ChassisPartSerial:   "vagrant",
+				BoardMfg:            "vagrant",
+				BoardMfgSerial:      "vagrant",
+				BoardPartNumber:     "vagrant",
+				ProductManufacturer: "vagrant",
+				ProductPartNumber:   "vagrant",
+				ProductSerial:       "vagrant",
+				FirmwareRevision:    "vagrant",
+			},
+			BIOS:         &api.BIOS{
+				Version: "0",
+				Vendor:  "vagrant",
+				Date: "01/01/2020",
+			},
+			Firmware:     0,
+		}
 	}
 	b.Vendor = api.GuessVendor(b.VendorString)
-	return b, nil
+	return b
 }
 
 // ConnectInBand will detect the board and choose the correct inband hal implementation
 func ConnectInBand(compliance api.Compliance) (hal.InBand, error) {
-	b, err := inBand()
-	if err != nil {
-		return nil, err
-	}
+	b := inBand()
 	switch b.Vendor {
 	case api.VendorLenovo:
 		return lenovo.InBand(b, compliance)
