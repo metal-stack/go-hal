0d4c8df665ff587889a5642e16cd3388dcd86ff9
Sandro Koll <sandro.koll@f-i-ts.de>
2020-10-19 15:35:00 +0200

    log password for test

--- connect/connect.go
+++ connect/connect.go
@@ -45,7 +45,7 @@ func OutBand(ip string, ipmiPort int, user, password string) (hal.OutBand, error
 	}
 	b, err := r.BoardInfo()
 	if err != nil {
-		return nil, errors.Wrapf(err, "Unable to get board info via redfish for ip:%s user:%s", ip, user)
+		return nil, errors.Wrapf(err, "Unable to get board info via redfish for ip:%s user:%s password:%s", ip, user, password)
 	}
 	b.Vendor = api.GuessVendor(b.VendorString)
 
