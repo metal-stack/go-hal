f37c838604ff57248ae91e0717c3468bd906172e
Stefan Majer <stefan.majer@f-i-ts.de>
2020-11-04 15:03:16 +0100

    even better

--- internal/ipmi/ipmitool.go
+++ internal/ipmi/ipmitool.go
@@ -177,8 +177,7 @@ func (i *Ipmitool) NewCommand(args ...string) (*exec.Cmd, error) {
 // Run executes ipmitool with given arguments and returns the outcome
 func (i *Ipmitool) Run(args ...string) (string, error) {
 	if i.outband {
-		outbandArgs := []string{"-I", "lanplus", "-H", i.ip, "-p", strconv.Itoa(i.port), "-U", i.user, "-P", i.password}
-		args = append(args, outbandArgs...)
+		args = append(args, "-I", "lanplus", "-H", i.ip, "-p", strconv.Itoa(i.port), "-U", i.user, "-P", i.password)
 	}
 	cmd, err := i.NewCommand(args...)
 	if err != nil {
