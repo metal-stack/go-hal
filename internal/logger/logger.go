56e4518dadfc11e5c56038416dc6b33d7b3f8967
Stefan Majer <stefan.majer@f-i-ts.de>
2020-11-11 08:25:01 +0100

    cosmetics

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -25,20 +25,14 @@ const (
 	Log15Logger
 )
 
-//Logger is our contract for the logger
+// Logger is our contract for the logger
 type Logger interface {
 	Debugf(format string, args ...interface{})
-
 	Infof(format string, args ...interface{})
-
 	Warnf(format string, args ...interface{})
-
 	Errorf(format string, args ...interface{})
-
 	Fatalf(format string, args ...interface{})
-
 	Panicf(format string, args ...interface{})
-
 	WithFields(keyValues Fields) Logger
 }
 
