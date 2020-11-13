2e831cb1eb4ace5b121ea202ba7f61811d19b4e7
Sandro Koll <sandro.koll@x-cellent.com>
2020-11-13 07:54:48 +0100

    Revert "fix vendor guessing bug (#29)"
    This reverts commit 89da0a8ee564fdc34ed3bca0b1d33fde46b5a31b.

--- pkg/api/types_test.go
+++ pkg/api/types_test.go
@@ -1,8 +1,6 @@
 package api
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestVendor_String(t *testing.T) {
 	tests := []struct {
@@ -19,30 +17,6 @@ func TestVendor_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.v.String(); got != tt.want {
 				t.Errorf("Vendor.String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGuessVendor(t *testing.T) {
-	tests := []struct {
-		name   string
-		vendor string
-		want   Vendor
-	}{
-		{name: "smc", vendor: "Supermicro", want: VendorSupermicro},
-		{name: "smc with space", vendor: " Supermicro ", want: VendorSupermicro},
-		{name: "lenovo", vendor: "Lenovo", want: VendorLenovo},
-		{name: "lenovolower", vendor: "lenovo", want: VendorLenovo},
-		{name: "empty", vendor: "", want: VendorUnknown},
-		{name: "unknown", vendor: "unknown", want: VendorUnknown},
-		{name: "vagrant", vendor: "vagrant", want: VendorVagrant},
-		{name: "dell", vendor: "Dell", want: VendorDell},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GuessVendor(tt.vendor); got != tt.want {
-				t.Errorf("GuessVendor() = %v, want %v", got, tt.want)
 			}
 		})
 	}
