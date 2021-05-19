e733162708ae643e223f99fa1905af1cf21c2495
Stefan Majer <stefan.majer@f-i-ts.de>
2021-05-19 10:24:21 +0200

    boot options are different

--- internal/vendors/supermicro/sum.go
+++ internal/vendors/supermicro/sum.go
@@ -193,7 +193,8 @@ func (s *sum) ConfigureBIOS() (bool, error) {
 		return false, err
 	}
 
-	if firmware == kernel.EFI && (s.machineType == X11SDV_8C_TP8F || s.secureBootEnabled) { // we cannot disable csm-support for S2 servers yet
+	// FIXME stefan check
+	if firmware == kernel.EFI && (s.machineType == X11SDV_8C_TP8F || s.machineType == X11DPT_B || s.secureBootEnabled) { // we cannot disable csm-support for S2 servers yet
 		return false, nil
 	}
 
